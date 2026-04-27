2d2933e22c547350095e806eb44d92150144e5ae
mirr-x <amir.exe@icloud.com>
2026-04-27 13:09:59 +0100

    Refactor main module to remove logging setup and improve exception handling

--- flyin/main.py
+++ flyin/main.py
@@ -1,5 +1,4 @@
-"""_summary_
-    This module is used t Excute and run the Fly-in project
+"""This module is used t Excute and run the Fly-in project
 
     Usage:
         python -m flyin.main <map_file>
@@ -8,28 +7,33 @@
             <map_file>: path of the map dron
 
 """
-import sys
-import logging
 
+import sys
 from flyin.parser.parser import Parser
+from flyin.parser import (
+    InvalidFormatError,
+    InvalidValueError,
+    InvalidCordsError,
+    ParserFileNotFoundError,
+    ParserError
+)
+
 
 
 def main() -> None:
     """ Entry point our app """
-    logging.info('Logging Started')
 
     print(sys.argv[3])  # delete me
-    parsing = Parser(sys.argv[3])
+    try:
+        parsing = Parser(sys.argv[3])
+    except 
 
 
 
 if __name__ == "__main__":
-    logging.basicConfig(
-        level=logging.INFO,
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    )
+
     try:
         main()
     except Exception as e:
-        logging.exception('Unhandled exception in main %s', e)
+        print('Unhandled exception in main %s', e)
         sys.exit(1)
