e3c448b40a08f60d17e248004ddd1a679d8f4cb5
mirr-x <amir.exe@icloud.com>
2026-04-28 11:36:09 +0100

    Refactor main module to improve logging and error handling

--- flyin/main.py
+++ flyin/main.py
@@ -9,31 +9,41 @@
 """
 
 import sys
+import logging
+from flyin.parser import _errors
 from flyin.parser.parser import Parser
-from flyin.parser import (
-    InvalidFormatError,
-    InvalidValueError,
-    InvalidCordsError,
-    ParserFileNotFoundError,
-    ParserError
-)
-
+from flyin.graph.graph import Graph
 
 
 def main() -> None:
     """ Entry point our app """
 
-    print(sys.argv[3])  # delete me
-    try:
-        parsing = Parser(sys.argv[3])
-    except 
+    logging.info('Logging Started\n')
+    if len(sys.argv) < 2:
+        logging.warning('Usage: python -m flyin.main <map_file>')
+        sys.exit(1)
 
+    try:
+        pars = Parser(sys.argv[1], map_graph=Graph())
+        pars.parsing()
+        # import json  ##* delet us
+        # with open('mp.json', 'w', encoding='utf-8') as f:   ##*
+        #     a = list(map(lambda z: z.__repr__(), pars.graph.zones.values()))
+        #     json.dump(a, f, indent=4)  ##*
+    except _errors.InsaneError as exc:
+        logging.critical('Graph Error: %s', exc)
+    except _errors.ParserError as exc:
+        logging.error('Map file error: %s (cause: %s)', exc, exc.__cause__)
+    logging.info('Parsing successful')
 
 
 if __name__ == "__main__":
-
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    )
     try:
         main()
-    except Exception as e:
-        print('Unhandled exception in main %s', e)
-        sys.exit(1)
+    except KeyboardInterrupt:
+        logging.exception('Execution interrupted by user')
+        sys.exit(130)
