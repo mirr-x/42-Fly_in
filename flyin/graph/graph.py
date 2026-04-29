121dc3d5d036880b94e5f8a06a63b00c9217e1b7
mirr-x <amir.exe@icloud.com>
2026-04-29 14:12:24 +0100

    Refactor Graph class to include Connection model and enhance type annotations

--- flyin/graph/graph.py
+++ flyin/graph/graph.py
@@ -1,6 +1,6 @@
 """Graph Module is the Module gonna store all data of the map and map itself"""
 
-from flyin.models import Zone
+from flyin.models import Zone, Connection
 from flyin.parser import _errors
 
 
@@ -8,7 +8,8 @@ class Graph:
     """Graph class store all info of the map"""
 
     def __init__(self) -> None:
-        self.zones = {}
+        self.zones: dict[str, Zone] = {}
+        self.connection: list[Connection] = []
 
     def add_zone(self, zone: Zone) -> None:
         """Add zone to the zones dict <ZoneRole>: Zone
@@ -23,3 +24,17 @@ class Graph:
         if not isinstance(zone, Zone):
             raise _errors.InsaneError('should append zones only!!!')
         self.zones[zone.name] = zone
+
+    def add_connection(self, connection: Connection) -> None:
+        """Add connection to the connection list
+
+        Args:
+            connection (Connection): connection to be add
+
+        Raises:
+            _errors.InsaneError: raised when not adding connection
+        """
+
+        if not isinstance(connection, Connection):
+            raise _errors.InsaneError('should append zones only!!!')
+        self.connection.append(connection)
