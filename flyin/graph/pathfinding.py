12626bfffd8e3f6f16ba1dd810c0324bc3d7c37e
mirr-x <amir.exe@icloud.com>
2026-05-04 17:59:48 +0100

    Refactor PathFind class constructor and initialize BFS algorithm in _bfs method

--- flyin/graph/pathfinding.py
+++ flyin/graph/pathfinding.py
@@ -2,12 +2,13 @@
 
 from flyin.models.zone import Zone
 from flyin.graph. graph import Graph
+import collections
 
 
 class PathFind:
     """Find the the shortest path using BFS algo"""
 
-    def __init__(self, graph: Graph | None = None) -> None:
+    def __init__(self, graph: Graph) -> None:
         self.graph = graph
 
     # TODO: now i will desing my own find_path()
@@ -26,4 +27,12 @@ class PathFind:
     # def _reconstruct_path(parent_map, start, goal) ->
 
     def _bfs(self, start: Zone, end: Zone) -> list[Zone]:
-        raise NotImplementedError
+        queue = collections.deque()
+        visited = set()
+        parent_map = dict()
+        queue.append(start)
+        while queue:
+            neighbors = self.graph.get_neighbors(queue[0])
+            visited.add(queue[0])
+            if neighbors:
+                
