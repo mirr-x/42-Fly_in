7a9e80981f891b5c65ae92aebbb26922c3731c7c
mirr-x <amir.exe@icloud.com>
2026-05-05 12:35:42 +0100

    Rename PathFind class to FindPath and implement BFS algorithm for pathfinding

--- flyin/graph/pathfinding.py
+++ flyin/graph/pathfinding.py
@@ -5,7 +5,7 @@ from flyin.graph. graph import Graph
 import collections
 
 
-class PathFind:
+class FindPath:
     """Find the the shortest path using BFS algo"""
 
     def __init__(self, graph: Graph) -> None:
@@ -26,13 +26,56 @@ class PathFind:
 
     # def _reconstruct_path(parent_map, start, goal) ->
 
-    def _bfs(self, start: Zone, end: Zone) -> list[Zone]:
+    def _bfs(self, start: Zone, end: Zone) -> None:  # -> list[Zone]
         queue = collections.deque()
         visited = set()
-        parent_map = dict()
+        parent_map = {}
+
         queue.append(start)
+        visited.add(queue[0])
         while queue:
-            neighbors = self.graph.get_neighbors(queue[0])
-            visited.add(queue[0])
-            if neighbors:
-                
+            parent = queue[0]
+            queue.popleft()
+            neighbors = self.graph.get_neighbors(parent)
+            for neighbor in neighbors:
+                if neighbor not in visited:
+                    visited.add(neighbor)
+                    queue.append(neighbor)
+                    parent_map[parent] = neighbor
+                    if neighbor == end:
+                        print("PATH FOUND!")
+                        print(parent_map)
+                        exit(1)
+    # TODO: Next you will dubug all this if all good an working on all maps and you will implemnt and skipp dead end and so an
+    # so we can finish phase 3
+
+
+# {
+#     start: [waypoint1],
+#     waypoint1: [start, waypoint2],
+#     waypoint2: [waypoint1, goal],
+#     goal: [waypoint2]
+# }
+#
+# queue = {start}
+# visited = {start}
+# map = {}
+#
+# loop 1:
+        # queue = {waypoint1}
+        # visited = {start}
+        # neighbors = [waypoint1]
+        # map = {start -> waypoint1}
+# loop 2:
+        # queue = {waypoint2}
+        # visited = {start, waypoint1}
+        # neighbors = [start, waypoint2]
+        # map = {start -> waypoint1,
+        #        waypoint1 -> waypoint2}
+# loop 3:
+        # queue = {goal}
+        # visited = {start, waypoint1, goal}
+        # neighbors = [waypoint1, goal]
+        # map = {start -> waypoint1,
+        #        waypoint1 -> waypoint2
+        #        waypoint2-> goal}
