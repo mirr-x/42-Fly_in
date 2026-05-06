beb436c4a5b9bcb7cae7b0c44423d03c22e20071
mirr-x <amir.exe@icloud.com>
2026-05-06 11:22:02 +0100

    Refactor FindPath class to implement find_shortest_path method and improve BFS logic

--- flyin/graph/pathfinding.py
+++ flyin/graph/pathfinding.py
@@ -1,7 +1,10 @@
 """Pathfinding module for computing shortest paths in a zone graph."""
 
+from typing import Optional
+
 from flyin.models.zone import Zone
-from flyin.graph. graph import Graph
+from flyin.graph.graph import Graph
+from flyin.parser import _errors
 import collections
 
 
@@ -11,8 +14,7 @@ class FindPath:
     def __init__(self, graph: Graph) -> None:
         self.graph = graph
 
-    # TODO: now i will desing my own find_path()
-    def find_path(self, start: Zone, end: Zone) -> list[Zone]:
+    def find_shortest_path(self, start: Zone, end: Zone) -> list[Zone]:
         """Return the shortest path from start to end.
 
         Args:
@@ -22,14 +24,33 @@ class FindPath:
         Returns:
             A list of zones representing the shortest path.
         """
-        raise NotImplementedError
+        parent_map = self._bfs(start, end)
+        if parent_map is None:
+            raise _errors.InvalidPathError('Path couldnt be find !!!')
+        return self._reconstruct_path(parent_map, start, end)
 
-    # def _reconstruct_path(parent_map, start, goal) ->
+    def _reconstruct_path(
+        self,
+        parent_map: dict[Zone, Zone],
+        start: Zone,
+        goal: Zone,
+    ) -> list[Zone]:
+        path = []
+        parent_map_reversed = dict(reversed(parent_map.items()))
+        cur = goal
+        for child, parent in parent_map_reversed.items():
+            if child == cur:
+                path.append(cur)
+                cur = parent
+        if cur == start:
+            path.append(cur)
+        path.reverse()
+        return path
 
-    def _bfs(self, start: Zone, end: Zone) -> dict[Zone, list[Zone]] | None:
+    def _bfs(self, start: Zone, end: Zone) -> Optional[dict[Zone, Zone]]:
         queue = collections.deque()
         visited = set()
-        parent_map = collections.defaultdict(list)
+        parent_map = {}
         path_found = False
 
         queue.append(start)
@@ -42,12 +63,12 @@ class FindPath:
                 if neighbor not in visited:
                     visited.add(neighbor)
                     queue.append(neighbor)
-                    parent_map[parent].append(neighbor)
+                    parent_map[neighbor] = parent
                     if neighbor == end:
                         path_found = True
         if path_found:
             print("PATH FOUND!")
-            print(dict(parent_map))
-        else:
-            print("PATH NOT VALID!!")
-        return dict(parent_map) if path_found else None
+            print(parent_map)
+        return parent_map if path_found else None
+
+# TODO: check witch GPT if this code is valid and all is good and its good with changes happend in subject ?? to move to phase 3
