cfae290a2cf000172ce6c37898c09175b87bd3d0
mirr-x <amir.exe@icloud.com>
2026-05-09 13:21:02 +0100

    Refactor _zone_validators.py to replace CostZone with ZoneCategory for consistency in cost handling

--- flyin/parser/_zone_validators.py
+++ flyin/parser/_zone_validators.py
@@ -1,6 +1,6 @@
 """This file containe internal helper functions for validation Parsing"""
 
-from flyin._types import CostZone, ColorZone, RoleZone, Cord, MetaData
+from flyin._types import ZoneCategory, ColorZone, RoleZone, Cord, MetaData
 from flyin.parser import _errors
 from flyin.models.zone import Zone
 
@@ -48,12 +48,12 @@ def _validate_cords(x: str, y: str, line_n: int) -> Cord:
     return (v1, v2)
 
 
-def _get_corect_cost(cost: str, line_n: int) -> CostZone:
+def _get_corect_cost(category: str, line_n: int) -> ZoneCategory:
     try:
-        return CostZone(cost)
+        return ZoneCategory(category)
     except ValueError as exc:
         raise _errors.InvalidValueError(
-            f'Unknowun zone cost At line {line_n}'
+            f'Unknowun zone category At line {line_n}'
         ) from exc
 
 
@@ -79,7 +79,7 @@ def _validate_meta_data(val: str, line_n: int) -> MetaData:
     for i in parts:
         key, _val = _validate_missing_key_val(i, '=', line_n)
         if key == 'zone':
-            meta_data['cost'] = _get_corect_cost(_val, line_n)
+            meta_data['category'] = _get_corect_cost(_val, line_n)
         elif key == 'color':
             meta_data['color'] = _get_corect_color(_val, line_n)
         elif key == 'max_drones':
