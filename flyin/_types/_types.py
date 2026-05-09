af8b2805cff496b6ded50fdd2968980e473dc0e5
mirr-x <amir.exe@icloud.com>
2026-05-09 13:20:33 +0100

    Replace CostZone with ZoneCategory in _types.py for consistency

--- flyin/_types/_types.py
+++ flyin/_types/_types.py
@@ -1,6 +1,6 @@
 """This file for Type define"""
 
-from flyin._types import CostZone, ColorZone, TypeAlias
+from flyin._types import ZoneCategory, ColorZone, TypeAlias
 
 Cord: TypeAlias = tuple[int, int]
-MetaData: TypeAlias = dict[str, CostZone | ColorZone | int]
+MetaData: TypeAlias = dict[str, ZoneCategory | ColorZone | int]
