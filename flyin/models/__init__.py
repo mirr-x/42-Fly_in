5e34ce51e90e9d19637b98a939f0648723852db1
mirr <121735620+mirr-x@users.noreply.github.com>
2026-05-11 06:36:49 +0000

    Remove unused imports and __all__ declaration from models __init__.py

--- flyin/models/__init__.py
+++ flyin/models/__init__.py
@@ -1,8 +1,3 @@
 # flyin/models/__init__.py
 
 """Internal file for Exporting the Importing"""
-
-from flyin.models.zone import Zone
-from flyin.models.connection import Connection
-
-__all__ = ['Zone', 'Connection']
