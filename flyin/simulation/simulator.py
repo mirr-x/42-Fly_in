b11b48f5935fbe3bce5088e99a7ace26586ebf6b
mirr <121735620+mirr-x@users.noreply.github.com>
2026-05-13 08:40:26 +0000

    Refactor Simulator class: implement in_transit functionality and improve connection handling

--- flyin/simulation/simulator.py
+++ flyin/simulation/simulator.py
@@ -1,11 +1,14 @@
 """Simulator module for simulating drone movement."""
 
 from flyin.models.drone import Drone
+from flyin.models.connection import Connection
+from flyin.models.zone import Zone
 from flyin.graph.graph import Graph
 from flyin.simulation.state import StateGraph
 from flyin._types._enums import DroneState
 
 
+# TODO: NEDS TESTINS CAUSE IVE JUST imlemented in_transit functunalty
 class Simulator:
     """Manages drone simulation and movement on a graph.
 
@@ -19,30 +22,36 @@ class Simulator:
         self.graph = graph
         self.state = StateGraph()
         self._init_all_drones_at_start_zone()
+        self._assign_paths_to_drones(self.drones)
 
     def _all_delivered(self) -> bool:
         return all(map(lambda d: d.is_delivered(), self.drones))
 
+    def _get_correct_cur_conn(
+            self, drone: Drone, next_zone: Zone | Connection | None
+            ) -> Connection | None:
+        if drone.state == DroneState.IN_TRANSIT:
+            return drone.current_zone
+        else:
+            return self.graph.get_connection(drone.current_zone, next_zone)
+
     def _process_turn(self, counter: int) -> str:
         movements = []
         for drone in self.drones:
-            next_zone = drone.next_zone()
-            curr_conn = self.graph.get_connection(
-                drone.current_zone, next_zone
-            )
-            if self.state.is_connection_occupied(curr_conn):
-                continue
             if drone.is_delivered():
+                continue
+            next_zone = drone.next_zone(self.graph)
+            cur_conn = self._get_correct_cur_conn(drone, next_zone)
+            if self.state.is_connection_occupied(cur_conn):
                 continue
             if self.state.is_zone_occupied(next_zone):
                 drone.set_status(DroneState.WAITING)
                 continue
-            # TODO: FIX alot of bugs here use ai  like free at end of turn 
             self.state.free_zone(drone.current_zone, drone)
-            self.state.free_connection(curr_conn, drone)
             self.state.reserve_zone(next_zone, drone)
-            self.state.reserve_connection(curr_conn, drone)
+            self.state.reserve_connection(cur_conn, drone)
             drone.move_next(next_zone)
+
             if next_zone:
                 movements.append(
                     f'{drone.id}-{drone.current_zone}'
@@ -55,10 +64,23 @@ class Simulator:
         for drone in self.drones:
             self.state.reserve_zone(start_zone, drone)
 
+    def _assign_paths_to_drones(self, drones: list[Drone]) -> None:
+        """Assign precomputed shortest paths to each drone."""
+
+        paths = self.graph.dijkstra_paths # hadi
+        l_paths = len(paths)
+        i = 0
+        for dron in drones:
+            if i == l_paths:
+                i = 0
+            dron.path = paths
+            i += 1
+
     def run(self) -> None:
         """Run the simulation."""
 
         counter = 1
         while not self._all_delivered():
             self._process_turn(counter)
+            self.state.reset_connection_usage()
             counter += 1
