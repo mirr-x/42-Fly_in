c888e49b8ce20a6185df593f66e757e65067060f
mirr <121735620+mirr-x@users.noreply.github.com>
2026-05-14 08:15:06 +0000

    Fix formatting in _get_zone_a_and_b and _is_connections_duplicate functions

--- flyin/parser/_connection_validator.py
+++ flyin/parser/_connection_validator.py
@@ -51,8 +51,8 @@ def _get_zone_a_and_b(val: str, line_n: int) -> tuple[str, str]:
         raise _errors.InvalidFormatError(
             f'Missing dash seprator in line {line_n}'
         )
-    parts[0] = (parts[0].strip()).lower()
-    parts[1] = (parts[1].strip()).lower()
+    parts[0] = parts[0].strip()
+    parts[1] = parts[1].strip()
     if not parts[0] or not parts[1]:
         raise _errors.InvalidFormatError(
             f'missing format <zone_A>-<zone_B> in line {line_n}')
@@ -69,7 +69,7 @@ def _is_connections_duplucate(
                                 zone_b: str,
                                 conn: list[Connection]
                             ) -> bool:
-    connctions = {frozenset([i.zone_a, i.zone_b]) for i in conn}
+    connctions = [frozenset([i.zone_a.name, i.zone_b.name]) for i in conn]
     return frozenset([zone_a, zone_b]) in connctions
 
 
