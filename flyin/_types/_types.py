efed09a2a9f2e71d5242a090f7612d84f07e603d
mirr <121735620+mirr-x@users.noreply.github.com>
2026-05-14 08:14:33 +0000

    Refactor type definitions: remove ColorZone from imports and update MetaData type

--- flyin/_types/_types.py
+++ flyin/_types/_types.py
@@ -1,6 +1,6 @@
 """This file for Type define"""
 
-from flyin._types import ZoneCategory, ColorZone, TypeAlias
+from flyin._types import ZoneCategory, TypeAlias
 
 Cord: TypeAlias = tuple[int, int]
-MetaData: TypeAlias = dict[str, ZoneCategory | ColorZone | int]
+MetaData: TypeAlias = dict[str, ZoneCategory | str | int]
