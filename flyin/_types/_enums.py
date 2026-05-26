f279ea89fe15b9fe90876fdfb6f5d096da3a8482
mirr-x <amir.exe@icloud.com>
2026-05-26 23:47:57 +0100

    Refactor ColorZone enum to use RGB tuples for color representation

--- flyin/_types/_enums.py
+++ flyin/_types/_enums.py
@@ -23,29 +23,28 @@ class RoleZone(Enum):
 class ColorZone(Enum):
     """This andicate the color of Zone"""
 
-    WHITE = 'white'
-    RED = 'red'
-    YELLOW = 'yellow'
-    CYAN = 'cyan'
-    GREEN = 'green'
-    BLUE = 'blue'
-    MAGENTA = 'magenta'
-    BLACK = 'black'
-    GRAY = 'gray'
-    ORANGE = 'orange'
-    PURPLE = 'purple'
-    PINK = 'pink'
-    BROWN = 'brown'
-    LIME = 'lime'
-    NAVY = 'navy'
-    TEAL = 'teal'
-    OLIVE = 'olive'
-    GOLD = 'gold'
-    MAROON = 'maroon'
-    DARKRED = 'darkred'
-    VIOLET = 'violet'
-    CRIMSON = 'crimson'
-    RAINBOW = 'rainbow'
+    WHITE = (255, 255, 255)
+    RED = (255, 0, 0)
+    YELLOW = (255, 255, 0)
+    CYAN = (0, 255, 255)
+    GREEN = (0, 255, 0)
+    BLUE = (0, 0, 255)
+    MAGENTA = (255, 0, 255)
+    BLACK = (0, 0, 0)
+    GRAY = (128, 128, 128)
+    ORANGE = (255, 165, 0)
+    PURPLE = (128, 0, 128)
+    PINK = (255, 192, 203)
+    BROWN = (165, 42, 42)
+    LIME = (50, 205, 50)
+    NAVY = (0, 0, 128)
+    TEAL = (0, 128, 128)
+    OLIVE = (128, 128, 0)
+    GOLD = (255, 215, 0)
+    MAROON = (128, 0, 0)
+    DARKRED = (139, 0, 0)
+    VIOLET = (238, 130, 238)
+    CRIMSON = (220, 20, 60)
 
 
 class MapsTool(Enum):
