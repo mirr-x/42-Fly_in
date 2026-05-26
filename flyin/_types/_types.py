4d75edfb483b7debd7b7dfc11474ea0078eeacfc
mirr-x <amir.exe@icloud.com>
2026-05-26 23:48:13 +0100

    Update MetaData type to include ColorZone for improved type safety

--- flyin/_types/_types.py
+++ flyin/_types/_types.py
@@ -1,6 +1,6 @@
 """This file for Type define"""
 
-from flyin._types import ZoneCategory, TypeAlias
+from flyin._types import ZoneCategory, TypeAlias, ColorZone
 
 Cord: TypeAlias = tuple[int, int]
-MetaData: TypeAlias = dict[str, ZoneCategory | str | int]
+MetaData: TypeAlias = dict[str, ZoneCategory | ColorZone | int]
